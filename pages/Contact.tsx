d5ca0a16c4454588c6aa6017a5c80fa0a2c27f84
sambhu nath sahu <sambhunathsahu95@gmail.com>
2025-02-17 11:25:37 +0530

    Add files via upload

---
+++ pages/Contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact: React.FC = () => {
+  return (
+    <div className="container">
+      <h1>Contact Us</h1>
+      <p>Welcome to the Contact Us page!</p>
+    </div>
+  );
+};
+
+export default Contact;
