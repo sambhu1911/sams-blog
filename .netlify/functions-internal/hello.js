37c4909f10d0647a8d0bd55043d41467b17fbab1
sambhu nath sahu <sambhunathsahu95@gmail.com>
2025-02-19 11:20:46 +0530

    Initial commit: add frontend and Netlify config

---
+++ .netlify/functions-internal/hello.js
@@ -0,0 +1,7 @@
+// filepath: netlify/functions/hello.js
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello from Netlify Functions!" })
+  };
+};
